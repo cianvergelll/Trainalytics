1ca2b9ba16c7f8f4ce76f41420f88e17788453d0
Ian <cianvergelll@gmail.com>
2026-01-01 20:51:36 +0800

    button adjustments icon, and static sample of journals in students

--- src/lib/components/SideNav.svelte
+++ src/lib/components/SideNav.svelte
@@ -48,7 +48,7 @@
 		<li
 			class="w-[95%] rounded-lg py-3 text-center text-lg font-bold transition duration-250 hover:scale-105 hover:bg-gray-100"
 		>
-			<a href="/saving-goals">Journals</a>
+			<a href="/student/journals">Journals</a>
 		</li>
 		<li
 			class="w-[95%] rounded-lg py-3 text-center text-lg font-bold transition duration-250 hover:scale-105 hover:bg-gray-100"
